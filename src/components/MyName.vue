1263ae4244620b9e1a32b9c4c02f245fd2361b6c
Zhang Junyi <jzha0539@student.monash.edu>
2025-09-06 17:48:32 +1000

    feat: auth + route guards + login

---
+++ src/components/MyName.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-name">
+    <h2>Hello, I'm Zhang Junyi!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+.my-name {
+  text-align: center;
+  margin-top: 20px;
+  font-size: 24px;
+  color: #42b983;
+}
+</style>
